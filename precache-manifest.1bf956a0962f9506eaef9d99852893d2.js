0dbfb65617a0656a10df9f40926c7b94c1baa4f7
animate-object <alexander.f.prinsen@gmail.com>
2020-02-25 00:28:47 -0600

    Updates

---
+++ precache-manifest.1bf956a0962f9506eaef9d99852893d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027a3a3ac3acd60e73ef250264e6d5f6",
+    "url": "/followed/index.html"
+  },
+  {
+    "revision": "ce646c5ed0b20f23c149",
+    "url": "/followed/static/css/main.e9eedc4d.chunk.css"
+  },
+  {
+    "revision": "e39facde0af017d59728",
+    "url": "/followed/static/js/2.55fbaa55.chunk.js"
+  },
+  {
+    "revision": "9d11307d0f4add8f1fb94ce38a4f8816",
+    "url": "/followed/static/js/2.55fbaa55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce646c5ed0b20f23c149",
+    "url": "/followed/static/js/main.0e6b847e.chunk.js"
+  },
+  {
+    "revision": "dcd92229e32e4ced06b2",
+    "url": "/followed/static/js/runtime-main.36d1aeb5.js"
+  }
+]);
